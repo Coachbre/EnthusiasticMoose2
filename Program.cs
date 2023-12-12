d60ad4ec0a2bd8570d3ee7c3012db3da270b7f59
Coachbre <coachbre.dev@gmail.com>
2023-12-12 13:01:34 -0500

    moose now asks a true or false question

--- Program.cs
+++ Program.cs
@@ -8,6 +8,35 @@ Console.WriteLine();
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 
+
+//call MooseAsks function
+bool isTrue = MooseAsks("Is Canada Real?");
+Console.WriteLine(isTrue);
+
+
+
+//ask a yes or no question and take answer (Y or N) and turn it to lowercase.
+//if answer is outside parameters, ask question again. Y= true and N=false
+bool MooseAsks(string question)
+{
+  Console.Write($"{question} (Y/N): ");
+  string answer = Console.ReadLine().ToLower();
+
+  while (answer != "y" && answer != "n")
+  {
+    Console.Write($"{question} (Y/N):");
+    answer = Console.ReadLine().ToLower();
+  }
+
+  if (answer == "y")
+  { return true;} else
+  { return false;}
+}
+
+
+
+
+
 // @ allows for a multiline string
 // $ allows for string interpolation
 void MooseSays(string message)
